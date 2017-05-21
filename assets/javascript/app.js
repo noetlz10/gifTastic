498a1803c40dab12f938bf654e1d279ed00376fc
Noe Tellez <noetlz10@gmail.com>
2017-05-21 15:07:13 -0500

    added https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
         $("#gifsAppear").empty();
         var searchInput = $(this).attr("data-name");
         //URL to search Giphy API
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             searchInput + "&api_key=dc6zaTOxFJmzC&limit=10";
         //Performing our AJAX GET request
         $.ajax({
